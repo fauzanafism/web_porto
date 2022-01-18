19ab4578659eb1a4295d4ff5550d4ec64c552eb0
fauzanafism <fauzan137@gmail.com>
2022-01-19 05:46:39 +0700

    renamed

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
-import 'package:web_porto/models/footer_item.dart';
+import 'package:web_porto/models/contact_item.dart';
 import 'package:web_porto/ui/constant.dart';
 import 'dart:html' as html;
 
@@ -264,16 +264,16 @@ class HomeScreen extends StatelessWidget {
                   children: const [
                     ResponsiveVisibility(
                       hiddenWhen: [Condition.smallerThan(name: TABLET)],
-                      child: FooterItem(
+                      child: ContactItem(
                           iconPath: 'res/images/mappin.png',
                           title: 'ADDRESS',
                           text1: 'Ciomas, Bogor'),
                     ),
-                    FooterItem(
+                    ContactItem(
                         iconPath: 'res/images/phone.png',
                         title: 'PHONE',
                         text1: '+62 852-1222-9257'),
-                    FooterItem(
+                    ContactItem(
                         iconPath: 'res/images/email.png',
                         title: 'EMAIL',
                         text1: 'fauzan137@gmail.com'),
