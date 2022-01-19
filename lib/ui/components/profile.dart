dd67d39daa3e70737543f262dbda3e97bd93d6bc
fauzanafism <fauzan137@gmail.com>
2022-01-19 10:13:38 +0700

    extracted widget to a new file

---
+++ lib/ui/components/profile.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:web_porto/ui/widgets/custom_button.dart';
+
+import '../constant.dart';
+
+class MyProfile extends StatelessWidget {
+  const MyProfile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      height: 650,
+      child: ResponsiveRowColumn(
+        columnMainAxisAlignment: MainAxisAlignment.spaceAround,
+        rowMainAxisAlignment: MainAxisAlignment.spaceAround,
+        rowCrossAxisAlignment: CrossAxisAlignment.center,
+        layout: ResponsiveWrapper.of(context).isSmallerThan('DESKTOP')
+            ? ResponsiveRowColumnType.COLUMN
+            : ResponsiveRowColumnType.ROW,
+        children: [
+          ResponsiveRowColumnItem(
+            columnOrder: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SelectableText(
+                  'FRESH GRADUATE',
+                  style: titleText.copyWith(color: Colors.grey),
+                ),
+                const SizedBox(height: 15),
+                SelectableText(
+                  'FAUZAN NAFIS\nMUHARAM',
+                  style: headText,
+                ),
+                const SizedBox(height: 15),
+                SelectableText(
+                  'Fresh graduate of IPB University, based in Bogor',
+                  style: bodyText.copyWith(color: Colors.white),
+                ),
+                const SizedBox(height: 15),
+                const CustomButton(
+                  urlPath:
+                      'https://drive.google.com/file/d/1AMeumJCZz1OWFU5LoEo0U4eoEXVCWCrR/view?usp=sharing',
+                  text: 'Download CV',
+                )
+              ],
+            ),
+          ),
+          ResponsiveRowColumnItem(
+            columnOrder: 1,
+            child: Stack(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(60),
+                      color: Colors.yellow),
+                ),
+                Positioned(
+                  bottom: 1,
+                  right: 1,
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(60),
+                        color: Colors.blue),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(125),
+                  child: const SizedBox(
+                    height: 250,
+                    width: 250,
+                    child: Image(
+                      fit: BoxFit.cover,
+                      image: AssetImage('res/images/self2.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 import 'package:web_porto/ui/components/app.dart';
 import 'package:web_porto/ui/constant.dart';
 import 'dart:html' as html;
 
 import 'components/contact.dart';
-import 'widgets/custom_button.dart';
+import 'components/profile.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -41,86 +40,7 @@ class HomeScreen extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 15),
-                height: 650,
-                child: ResponsiveRowColumn(
-                  columnMainAxisAlignment: MainAxisAlignment.spaceAround,
-                  rowMainAxisAlignment: MainAxisAlignment.spaceAround,
-                  rowCrossAxisAlignment: CrossAxisAlignment.center,
-                  layout: ResponsiveWrapper.of(context).isSmallerThan('DESKTOP')
-                      ? ResponsiveRowColumnType.COLUMN
-                      : ResponsiveRowColumnType.ROW,
-                  children: [
-                    ResponsiveRowColumnItem(
-                      columnOrder: 2,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SelectableText(
-                            'FRESH GRADUATE',
-                            style: titleText.copyWith(color: Colors.grey),
-                          ),
-                          const SizedBox(height: 15),
-                          SelectableText(
-                            'FAUZAN NAFIS\nMUHARAM',
-                            style: headText,
-                          ),
-                          const SizedBox(height: 15),
-                          SelectableText(
-                            'Fresh graduate of IPB University, based in Bogor',
-                            style: bodyText.copyWith(color: Colors.white),
-                          ),
-                          const SizedBox(height: 15),
-                          const CustomButton(
-                            urlPath:
-                                'https://drive.google.com/file/d/1AMeumJCZz1OWFU5LoEo0U4eoEXVCWCrR/view?usp=sharing',
-                            text: 'Download CV',
-                          )
-                        ],
-                      ),
-                    ),
-                    ResponsiveRowColumnItem(
-                      columnOrder: 1,
-                      child: Stack(
-                        // mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 120,
-                            width: 120,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(60),
-                                color: Colors.yellow),
-                          ),
-                          Positioned(
-                            bottom: 1,
-                            right: 1,
-                            child: Container(
-                              height: 120,
-                              width: 120,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(60),
-                                  color: Colors.blue),
-                            ),
-                          ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(125),
-                            child: const SizedBox(
-                              height: 250,
-                              width: 250,
-                              child: Image(
-                                fit: BoxFit.cover,
-                                image: AssetImage('res/images/self2.png'),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
+              const MyProfile(),
               const SizedBox(
                 height: 50,
               ),
@@ -159,3 +79,4 @@ class HomeScreen extends StatelessWidget {
         ));
   }
 }
+
