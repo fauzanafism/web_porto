eeb3200f72a5274a1b21e2d8b9e9115227237939
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-11-17 17:16:10 +0700

    added new optional property

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:web_porto/ui/widgets/custom_button.dart';
+import 'dart:html' as html;
 
 import '../constant.dart';
 
@@ -11,32 +12,31 @@ class AppSection extends StatelessWidget {
   final String appName;
   final String description;
   final String gitUrl;
+  final bool isPlayStore;
 
-  const AppSection({
-    Key? key,
-    required this.imageFirst,
-    required this.imagePath,
-    required this.platformName,
-    required this.appName,
-    required this.description,
-    required this.gitUrl
-  }) : super(key: key);
+  const AppSection(
+      {Key? key,
+      required this.imageFirst,
+      required this.imagePath,
+      required this.platformName,
+      required this.appName,
+      required this.description,
+      required this.gitUrl,
+      this.isPlayStore = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: ResponsiveValue(context,
-          defaultValue: 650.0,
-          valueWhen: const [
-            Condition.smallerThan(name: 'DESKTOP2', value: 810.0)
-          ]).value,
+      height: ResponsiveValue(context, defaultValue: 650.0, valueWhen: const [
+        Condition.smallerThan(name: 'DESKTOP2', value: 810.0)
+      ]).value,
       margin: const EdgeInsets.symmetric(horizontal: 10),
       child: ResponsiveRowColumn(
         rowMainAxisAlignment: MainAxisAlignment.center,
-        layout:
-            ResponsiveWrapper.of(context).isSmallerThan('DESKTOP2')
-                ? ResponsiveRowColumnType.COLUMN
-                : ResponsiveRowColumnType.ROW,
+        layout: ResponsiveWrapper.of(context).isSmallerThan('DESKTOP2')
+            ? ResponsiveRowColumnType.COLUMN
+            : ResponsiveRowColumnType.ROW,
         children: [
           ResponsiveRowColumnItem(
             rowOrder: imageFirst ? 1 : 3,
@@ -44,7 +44,10 @@ class AppSection extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
               child: SizedBox(
                 width: 599,
-                child: Image.asset(imagePath, fit: BoxFit.cover,),
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
@@ -76,7 +79,16 @@ class AppSection extends StatelessWidget {
                   style: bodyText.copyWith(color: Colors.white),
                 ),
                 const SizedBox(height: 15),
-                CustomButton(text: 'EXPLORE MORE', urlPath: gitUrl)
+                (isPlayStore)
+                    ? MouseRegion(
+                        child: GestureDetector(
+                          child: Image.asset('res/images/playbadge.png'),
+                          onTap: () {
+                            html.window.open(gitUrl, "_blank");
+                          },
+                        ),
+                      )
+                    : CustomButton(text: 'EXPLORE MORE', urlPath: gitUrl)
               ],
             ),
           ),
